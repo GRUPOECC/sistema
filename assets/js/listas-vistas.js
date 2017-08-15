313ee88dc216c6c1c8985abc0d103ad3d15ca8ba
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-08-15 17:31:07 -0400

    Agregando libreria para switch de bootstrap y codigo javascript de cuadriculas

---
+++ assets/js/listas-vistas.js
@@ -0,0 +1,62 @@
+
+//Codigo implementado por: Garry Jr. Bruno 
+//--------------------------------------------------------------------------
+//Descripcion de la funcion:
+/**
+  Se encarga de gestionar la manera en como se ven los listados de datos 
+  en el sitio web. Esta puede verse en forma de lista o en forma de cuadriculas.
+
+**/
+
+//Indicador de tipo de listado activa:
+//Vale 1 si esta activo el listado en modo lista 
+//Vale 2 si esta activo el listado en modo cuadricula 
+
+
+//Inizializo variables:
+//----------------------------------------------------------
+var x =0;  //Vale 0 por que es el estado inicial del listado
+//Inilizo una lista por defecto:
+listaporDefecto();
+//----------------------------------------------------------
+
+function cambioVista() {
+      
+    if (x==1){
+         document.getElementById('modo-lista').style.display = "none";
+         document.getElementById('modo-cuadricula').style.display = "block";
+         x=2;
+	} else if (x==2){
+         document.getElementById('modo-lista').style.display = "block";
+         document.getElementById('modo-cuadricula').style.display = "none";
+         x=1;
+	} 
+
+}
+
+function listaporDefecto(){
+         document.getElementById('modo-lista').style.display = "block";
+         document.getElementById('modo-cuadricula').style.display = "none";
+         x=1;
+}
+
+
+//-----------------------------------------------------------------------------------
+//Proceso encargado de la busqueda de datos el listado de Contactos: 
+
+    var search = document.getElementById("search"),
+    listado = document.getElementById("resultados"),
+    contacto = listado.getElementsByTagName("p"),
+    forEach = Array.prototype.forEach;
+
+    search.addEventListener("keyup", function(e){
+    var choice = this.value;
+  
+    forEach.call(contacto, function(f){
+        if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
+            f.parentNode.style.display = "none";        
+        else
+            f.parentNode.style.display = "block";        
+    });
+    }, false);
+//------------------------------------------------------------------------------------
